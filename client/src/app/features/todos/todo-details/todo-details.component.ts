c53caa7da3f7cffa10cd889c4b52bc920f05a3df
KryptonBD <foysol.sahi@gmail.com>
2025-02-22 13:54:31 +0100

    feat: design todo details

--- client/src/app/features/todos/todo-details/todo-details.component.ts
+++ client/src/app/features/todos/todo-details/todo-details.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Todo } from '../../../../shared/models/Todo';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-details.component.html',
-  styleUrl: './todo-details.component.scss'
+  styleUrl: './todo-details.component.scss',
 })
 export class TodoDetailsComponent {
+  private router = inject(Router);
+  protected todo = signal<Todo | null>(null);
 
+  protected goBack() {
+    this.router.navigate(['/']);
+  }
+
+  protected edit() {
+    this.router.navigate(['todos', this.todo()?.id, 'edit']);
+  }
 }
