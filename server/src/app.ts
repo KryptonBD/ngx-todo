dcf0358a19639840fcf402eb809ad62efaa9d82c
KryptonBD <foysol.sahi@gmail.com>
2025-02-23 22:40:35 +0100

    feat: add angular environments instead of proxy

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { AppDataSource } from "./config/db";
 import todos from "./routes/todos.route";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 AppDataSource.initialize()
   .then(() => {
